a9046c47a0bd91a5e618845e7cad2f1b62be5f8c
angela chaj <angelachaj648@gmail.com>
2026-05-18 11:30:24 -0600

    Agregar botones

---
+++ Form3.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Juego_de_Estrategias
+{
+	partial class Form3
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			textBox1 = new TextBox();
+			textBox2 = new TextBox();
+			button1 = new Button();
+			button2 = new Button();
+			button3 = new Button();
+			button4 = new Button();
+			SuspendLayout();
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(536, 143);
+			textBox1.Name = "textBox1";
+			textBox1.Size = new Size(125, 27);
+			textBox1.TabIndex = 0;
+			textBox1.Text = "salir";
+			textBox1.TextChanged += textBox1_TextChanged;
+			// 
+			// textBox2
+			// 
+			textBox2.Location = new Point(515, 91);
+			textBox2.Name = "textBox2";
+			textBox2.Size = new Size(125, 27);
+			textBox2.TabIndex = 1;
+			textBox2.Text = "menu";
+			// 
+			// button1
+			// 
+			button1.Location = new Point(173, 130);
+			button1.Name = "button1";
+			button1.Size = new Size(94, 29);
+			button1.TabIndex = 2;
+			button1.Text = "button1";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			button2.Location = new Point(173, 223);
+			button2.Name = "button2";
+			button2.Size = new Size(94, 29);
+			button2.TabIndex = 3;
+			button2.Text = "Reglas";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(173, 313);
+			button3.Name = "button3";
+			button3.Size = new Size(94, 29);
+			button3.TabIndex = 4;
+			button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			button4.Location = new Point(173, 404);
+			button4.Name = "button4";
+			button4.Size = new Size(94, 29);
+			button4.TabIndex = 5;
+			button4.Text = "button4";
+			button4.UseVisualStyleBackColor = true;
+			// 
+			// Form3
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(671, 528);
+			Controls.Add(button4);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(button1);
+			Controls.Add(textBox2);
+			Controls.Add(textBox1);
+			Name = "Form3";
+			Text = "Form3";
+			Load += Form3_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private TextBox textBox1;
+		private TextBox textBox2;
+		private Button button1;
+		private Button button2;
+		private Button button3;
+		private Button button4;
+	}
+}
